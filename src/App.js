35bff7a3e1f0e9874edffb00972ab2d9d9de69db
Aditya Khandelwal <khandelwaladitya410@gmail.com>
2023-07-22 11:55:45 +0530

    removing unnecessary onclicks

--- src/App.js
+++ src/App.js
@@ -1,80 +1,105 @@
-import React, { useState } from 'react';
+// @ts-nocheck
+import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [result, setResult] = useState("");
 
-  const [result,setResult] = useState("");
-
-  const handleClick = (event)=>{
+  const handleByTag = (event) => {
     event.stopPropagation();
-    console.log("button is clicked!");
-    setResult(result.concat(event.target.name));
-  }
-
-  const handleClear = (event)=>{
-    event.stopPropagation();
-    console.log("clear button is clicked!");
-    setResult("");
-  }
-  
-  const handleBackSpace = (event)=>{
-    event.stopPropagation();
-    console.log("backspace button is clicked!");
-    setResult(result.slice(0,result.length-1));
-  }
-
-  const handleCalculate = (event)=>{
-    event.stopPropagation();
-    console.log("calculate button is clicked!");
-    setResult(eval(result).toString());
-  }
-
-  const handleWrapper = (event)=>{
-    event.stopPropagation();
-    console.log("wrapper is clicked!");
-    // setResult(eval(result).toString());
-  }
+    if (event.target.name === "Clear") {
+      setResult("");
+    } else if (event.target.name === "C") {
+      setResult(result.slice(0, result.length - 1));
+    } else if (event.target.name === "=") {
+      setResult(eval(result).toString());
+    } else if (event.target.tagName === "BUTTON") {
+      setResult(result.concat(event.target.name));
+    }
+  };
 
   return (
     <>
-    <div className="container mx-auto flex flex-col items-center" onClick={handleWrapper}>
+      <div
+        className="container mx-auto flex flex-col items-center"
+        onClick={handleByTag}
+      >
         <div className="row">
-            <input className="input" type="text" value={result}/>
+          <input className="input" type="text" value={result} />
         </div>
         <div className="row">
-            <button name = "Clear" className="button" id = "clear" onClick={handleClear}>Clear</button>
-            <button name = "C" className="button" id = "backspace" onClick={handleBackSpace}>C</button>
-            <button name = "%" className="button" onClick={handleClick}>%</button> 
-            <button name = "M-" className="button" onClick={handleClick}>M-</button>
+          <button name="Clear" className="button" id="clear">
+            Clear
+          </button>
+          <button name="C" className="button" id="backspace">
+            C
+          </button>
+          <button name="%" className="button">
+            %
+          </button>
+          <button name="M-" className="button">
+            M-
+          </button>
         </div>
         <div className="row">
-            <button name = "7" className="button" onClick={handleClick}>7</button>
-            <button name = "8" className="button" onClick={handleClick}>8</button>
-            <button name = "9" className="button" onClick={handleClick}>9</button>
-            <button name = "*" className="button" onClick={handleClick}>*</button>
+          <button name="7" className="button">
+            7
+          </button>
+          <button name="8" className="button">
+            8
+          </button>
+          <button name="9" className="button">
+            9
+          </button>
+          <button name="*" className="button">
+            *
+          </button>
         </div>
         <div className="row">
-            <button name = "4" className="button" onClick={handleClick}>4</button>
-            <button name = "5" className="button" onClick={handleClick}>5</button>
-            <button name = "6" className="button" onClick={handleClick}>6</button>
-            <button name = "/" className="button" onClick={handleClick}>/</button>
+          <button name="4" className="button">
+            4
+          </button>
+          <button name="5" className="button">
+            5
+          </button>
+          <button name="6" className="button">
+            6
+          </button>
+          <button name="/" className="button">
+            /
+          </button>
         </div>
         <div className="row">
-            <button name = "1" className="button" onClick={handleClick}>1</button>
-            <button name = "2" className="button" onClick={handleClick}>2</button>
-            <button name = "3" className="button" onClick={handleClick}>3</button>
-            <button name = "+" className="button" onClick={handleClick}>+</button>
+          <button name="1" className="button">
+            1
+          </button>
+          <button name="2" className="button">
+            2
+          </button>
+          <button name="3" className="button">
+            3
+          </button>
+          <button name="+" className="button">
+            +
+          </button>
         </div>
         <div className="row">
-            <button name = "0" className="button" onClick={handleClick}>0</button>
-            <button name = "." className="button" onClick={handleClick}>.</button>
-            <button name = "=" className="button" onClick={handleCalculate}>=</button>
-            <button name = "-" className="button" onClick={handleClick}>-</button>
+          <button name="0" className="button">
+            0
+          </button>
+          <button name="." className="button">
+            .
+          </button>
+          <button name="=" className="button">
+            =
+          </button>
+          <button name="-" className="button">
+            -
+          </button>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
-
+  );
+};
 
 export default App;
